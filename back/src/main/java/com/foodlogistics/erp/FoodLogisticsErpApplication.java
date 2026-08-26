6d6b387a65f72bba439e62e771a7738237ec01f0
neukdol30-ai <neukdol30@gmail.com>
2026-08-26 10:54:59 +0900

    버그수정

--- back/src/main/java/com/foodlogistics/erp/FoodLogisticsErpApplication.java
+++ back/src/main/java/com/foodlogistics/erp/FoodLogisticsErpApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.foodlogistics.erp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo1Application {
+public class FoodLogisticsErpApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Demo1Application.class, args);
+        SpringApplication.run(FoodLogisticsErpApplication.class, args);
     }
 
 }
--- back/src/test/java/com/foodlogistics/erp/FoodLogisticsErpApplicationTests.java
+++ back/src/test/java/com/foodlogistics/erp/FoodLogisticsErpApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.foodlogistics.erp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Demo1ApplicationTests {
+class FoodLogisticsErpApplicationTests {
 
     @Test
     void contextLoads() {
